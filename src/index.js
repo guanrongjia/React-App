aaf7cba2878da796e9e6d3929a7d2f88083a6ca7
guanrongjia <rjguan@ctrip.com>
2020-07-31 15:05:32 +0800

    test @ctrip/pay

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { VerifyCodePanel } from "@ctrip/pay";
 
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <div>
+    <VerifyCodePanel
+      title="手机验证"
+      subTitle="已向您的绑定手机136****5678发送短信验证码"
+      length={6}
+      autoConfirm={true}
+      onSubmit={() => {
+        console.log("submit");
+      }}
+      getCode={() => {
+        console.log("getCode");
+      }}
+      visiable={true}
+      timerCount={120}
+      timerTitle="重新获取"
+      timerEnd={() => {
+        console.log("timerEnd");
+      }}
+      onChangeText={() => {
+        console.log("onChangeText");
+      }}
+      buttonText="提交"
+      onClose={() => {
+        setVisiable(false);
+      }}
+    />
+  </div>,
   document.getElementById("root")
-)
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,81 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    app: "./src/index.js"
+    app: "./src/index.js",
   },
   output: {
     filename: "[hash].bundle.js",
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, "build"),
   },
   devServer: {
     contentBase: path.join(__dirname, "public"),
     historyApiFallback: true,
     inline: true,
     hot: true,
-    port: 3000,
+    port: 8238,
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.js|jsx$/,
-        use: ['babel-loader'],
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
+        use: ["babel-loader"],
+        exclude: /(node_modules|bower_components)/,
+      },
+
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+          {
+            loader: "file-loader",
+            options: {
+              name: "./images/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'React App',
-      filename: 'index.html',
-      template: 'public/index.html',
-      favicon: 'public/favicon.ico',
+      title: "React App",
+      filename: "index.html",
+      template: "public/index.html",
+      favicon: "public/favicon.ico",
       inject: true,
-      minify: { //压缩HTML文件
-        removeComments: true,//移除HTML中的注释
-        collapseWhitespace: true //删除空白符与换行符
-      }
-    })
-  ]
-}
+      minify: {
+        //压缩HTML文件
+        removeComments: true, //移除HTML中的注释
+        collapseWhitespace: true, //删除空白符与换行符
+      },
+    }),
+  ],
+};
